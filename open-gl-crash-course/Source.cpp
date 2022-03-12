7a6fb789e8e9adb6cdc3904ee71445e20e8cf61c
QuangPham <quang20201@gmail.com>
2022-03-12 22:14:02 +0700

    Add project files.

---
+++ open-gl-crash-course/Source.cpp
@@ -0,0 +1,28 @@
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include <iostream>
+
+
+int main() {
+	glfwInit();
+	/*
+		Configuring GLFW:
+		Use OpenGL version 3.3
+		Use the core-profile
+	*/
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create GLFW window object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Create the window context
+	glfwMakeContextCurrent(window);
+
+	return 0;
+}
