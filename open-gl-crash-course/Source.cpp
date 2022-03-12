3c421438e303948f3c079d5252e200c7e2e12b18
QuangPham <quang20201@gmail.com>
2022-03-12 23:58:04 +0700

    clear window

--- open-gl-crash-course/Source.cpp
+++ open-gl-crash-course/Source.cpp
@@ -2,9 +2,11 @@
 #include<GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main() {
 	glfwInit();
@@ -18,7 +20,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create GLFW window object
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -26,18 +28,39 @@ int main() {
 	}
 	// Create the window context
 	glfwMakeContextCurrent(window);
-
+	// Adjust the Viewport as the window is resized by registering the window to the callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
 	// Init OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to init GLAD" << std::endl;
 		return -1;
 	}
 
-	// Set OpenGL Viewport
-	glViewport(0, 0, 800, 600);
+	// Render loop
+	while(!glfwWindowShouldClose(window)) {
+		// Close GLFW when pressing Escape key
+		processInput(window);
 
-	// Adjust the Viewport as the window is resized by registering the window to the callback
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+		// Rendering stuffs here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // state-setting function
+		glClear(GL_COLOR_BUFFER_BIT); // state-using function
 
+		glfwSwapBuffers(window);
+		// Trigger keyboard input or mouse events => update window state
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
