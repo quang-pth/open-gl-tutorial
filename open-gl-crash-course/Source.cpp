daddf02fb9fe8fbf3c2548f7ea22e8a98b24e122
QuangPham <quang20201@gmail.com>
2022-03-13 23:57:39 +0700

    create vertex shader object

--- open-gl-crash-course/Source.cpp
+++ open-gl-crash-course/Source.cpp
@@ -7,6 +7,12 @@ void processInput(GLFWwindow* window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout(location = 0) in vec3 aPos;\n"
+	"void main() {\n"
+	"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+	"}\0"
+;
 
 int main() {
 	glfwInit();
@@ -45,6 +51,33 @@ int main() {
 		// Rendering stuffs here
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // state-setting function
 		glClear(GL_COLOR_BUFFER_BIT); // state-using function
+
+		// Vertices Data
+		float vertices[] = {
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.0f, 0.5f, 0.0f,
+		};
+		// Create Vertex Buffer Object (VBO) to manage Vertices Data
+		unsigned int VBO;
+		glGenBuffers(1, &VBO); // generate VBO's id
+		glBindBuffer(GL_ARRAY_BUFFER, VBO); // bound VBO object
+		// Store Vertex Data within mermory of the GPU as managed by VBO 
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		// Vertex Shader object
+		unsigned int vertexShader;
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		// Attach vertex shader source to shader object
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
+		// Check if vertex shader is rendered successfully
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			std::cout << "ERROR:SHADER:VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
 
 		glfwSwapBuffers(window);
 		// Trigger keyboard input or mouse events => update window state
