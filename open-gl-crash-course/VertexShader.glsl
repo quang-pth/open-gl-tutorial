bb62ee42169d48b2b0cde260fbd96df1cad762a3
QuangPham <quang20201@gmail.com>
2022-03-23 14:23:32 +0700

    exercise 6.8 1.triangle upside down

--- open-gl-crash-course/VertexShader.glsl
+++ open-gl-crash-course/VertexShader.glsl
@@ -6,6 +6,6 @@ layout (location = 1) in vec3 aColor; // color attribute
 out vec3 ourColor; 
 
 void main() {
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0);
 	ourColor = aColor;
 }
