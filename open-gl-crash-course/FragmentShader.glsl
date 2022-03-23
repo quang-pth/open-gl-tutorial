507a679874b6d54021999110c3b57c727cdf698d
QuangPham <quang20201@gmail.com>
2022-03-23 17:52:29 +0700

    finish shader chapter

--- open-gl-crash-course/FragmentShader.glsl
+++ open-gl-crash-course/FragmentShader.glsl
@@ -3,9 +3,7 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
-in vec4 vertexPosition;
 
 void main() {
-	// FragColor = vec4(ourColor, 1.0);
-	FragColor = vertexPosition;
+	FragColor = vec4(ourColor, 1.0);
 }
--- open-gl-crash-course/VertexShader.glsl
+++ open-gl-crash-course/VertexShader.glsl
@@ -3,11 +3,9 @@
 layout (location = 0) in vec3 aPos; // position attribute
 layout (location = 1) in vec3 aColor; // color attribute
 
-// out vec3 ourColor;
-out vec4 vertexPosition;
+out vec3 ourColor;
 
 void main() {
 	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); // add xOffset to move triangle by the x position
-	// ourColor = aColor;
-	vertexPosition = gl_Position;
+	ourColor = aColor;
 }
