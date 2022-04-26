cb1a070d44023b1071eca391617e06e24bc35b74
QuangPham <quang20201@gmail.com>
2022-04-26 14:54:19 +0700

    execerse 13.7.3 world space to view space

--- open-gl-crash-course/VertexShader.glsl
+++ open-gl-crash-course/VertexShader.glsl
@@ -7,12 +7,16 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+uniform vec3 lightPos;
+
 out vec3 Normal;
 out vec3 FragPos;
+out vec3 LightPos;
 
 void main() {
 	gl_Position = projection * view * model * vec4(aPos, 1.0); 
-	FragPos = vec3(model * vec4(aPos, 1.0));
+	FragPos = vec3(view * model * vec4(aPos, 1.0));
 	// Calc normal matrix to convert normal vector to world space, cast it to 3x3 matrix to get only the linear transform part
-	Normal = mat3(transpose(inverse(model))) * aNormal; 
+	Normal = mat3(transpose(inverse(view * model))) * aNormal;
+	LightPos = vec3(view * vec4(lightPos, 1.0));
 }
