5a9683d4368217df429109f23817548d0add0b42
QuangPham <quang20201@gmail.com>
2022-04-26 14:39:54 +0700

    execise 13.7.2

--- open-gl-crash-course/FragmentShader.glsl
+++ open-gl-crash-course/FragmentShader.glsl
@@ -12,7 +12,7 @@ uniform vec3 viewPos;
 
 void main() {
 	// constant ambient factor
-	float ambientStrength = 0.1;
+	float ambientStrength = 0.2;
 	vec3 ambient = ambientStrength * lightColor;
 
 	// Calc Diffuse component
@@ -22,10 +22,10 @@ void main() {
 	vec3 diffuse = diffuseFactor * lightColor; // light is affected by the diffuse component
 
 	// Calc Specular component
-	float specularStrength = 0.5;
+	float specularStrength = 0.8; // idensity of the light spot in the object surface
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, normal); // negate the light direction because we need a light direction from light source to the fragment
-	float specularFactor = pow(max(dot(viewDir, reflectDir), 0), 32); // 32 is the shininess value
+	float specularFactor = pow(max(dot(viewDir, reflectDir), 0), 256); // 32 is the shininess value
 	vec3 specular = specularStrength * specularFactor * lightColor;
 
 	// Combine ambient and diffuse and specular component to create object color
