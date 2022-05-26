407c324cea69ec104567c2238a557f90d95e8ae4
QuangPham <quang20201@gmail.com>
2022-05-26 22:41:48 +0700

    exercise reverse face culling

--- open-gl-crash-course/Source.cpp
+++ open-gl-crash-course/Source.cpp
@@ -89,7 +89,7 @@ int main() {
 	// Enable face culling
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK /*discard back-facing fragments*/);
-	glFrontFace(GL_CCW /*set triangle with counter-clockwise winding order as front-facing face*/);
+	glFrontFace(GL_CW /*set triangle with counter-clockwise winding order as front-facing face*/);
 
 	// Light source shader
 	Shader shader("depth-test-vertex.glsl", "depth-test-fragment.glsl");
@@ -98,46 +98,46 @@ int main() {
 	float cubeVertices[] = {
 		// back face
 		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // bottom-left
-		 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-right
 		 0.5f, -0.5f, -0.5f, 1.0f, 0.0f, // bottom-right
 		 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-right
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // bottom-left
+		 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-right
 		-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // top-left
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // bottom-left
 		// front face
 		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, // bottom-left
+		 0.5f,  0.5f, 0.5f, 1.0f, 1.0f, // top-right
 		 0.5f, -0.5f, 0.5f, 1.0f, 0.0f, // bottom-right
 		 0.5f,  0.5f, 0.5f, 1.0f, 1.0f, // top-right
-		 0.5f,  0.5f, 0.5f, 1.0f, 1.0f, // top-right
-		-0.5f,  0.5f, 0.5f, 0.0f, 1.0f, // top-left
 		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, // bottom-left
+		-0.5f,  0.5f, 0.5f, 0.0f, 1.0f, // top-left
 		// left face
 		-0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-right
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-left
 		-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-left
 		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-left
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-left
-		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-right
 		-0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-right
+		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-right
 		// right face
 		0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-left
-		0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-right
 		0.5f,  0.5f, -0.5f, 1.0f, 1.0f, // top-right
 		0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-right
-		0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-left
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // bottom-right
 		0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-left
+		0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // top-left
 		// bottom face
 		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // top-right
+		 0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // bottom-left
 		 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, // top-left
 		 0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // bottom-left
-		 0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // bottom-left
-		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-right
 		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // top-right
+		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, // bottom-right
 		// top face
 		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, // top-left
 		 0.5f, 0.5f,  0.5f, 1.0f, 0.0f, // bottom-right
+		-0.5f, 0.5f,  0.5f, 0.0f, 0.0f, // bottom-left
 		 0.5f, 0.5f, -0.5f, 1.0f, 1.0f, // top-right
 		 0.5f, 0.5f,  0.5f, 1.0f, 0.0f, // bottom-right
 		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, // top-left
-		-0.5f, 0.5f,  0.5f, 0.0f, 0.0f // bottom-left
 	};
 	float planeVertices[] = {
 		// positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
